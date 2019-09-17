3539a6ac9f988049b741e15f670953287c20aa90
Leyla Nazhandali <leyla@vt.edu>
2019-09-17 18:46:12 -0400

    modifications

--- struct_example_main.c
+++ struct_example_main.c
@@ -5,6 +5,7 @@ typedef struct {
     char c;
 } bunch_t;
 
+//This function uses pass-by-reference
 void increaseBunch_p(bunch_t *inputBunch_p)
 {
     inputBunch_p->x = inputBunch_p->x + 1;
@@ -13,6 +14,7 @@ void increaseBunch_p(bunch_t *inputBunch_p)
     inputBunch_p->c = inputBunch_p->c + 1;
 }
 
+// This function uses pass-by-value
 bunch_t increaseBunch(bunch_t inputBunch)
 {
     bunch_t outputBunch;
@@ -26,8 +28,26 @@ bunch_t increaseBunch(bunch_t inputBunch)
 
 int main(void)
 {
+    int a = 5;
+    int *a_p = 0;  //a_p is a pointer to the type int. I assign 0 to it to make it a null pointer which means it is pointing to nowhere.
+
+    a_p = &a; //& operator is the reference operator. a_p is going to point to a
+
+    a = 7; //directly write to the variable.
+
+    *a_p = 8; // using the pointer to write to the variable. * is dereferencing the pointer.
+
+
     bunch_t myBunch = {1, 2, 3, '7'};
     bunch_t newBunch = increaseBunch(myBunch);
+
+    newBunch.c = 5;
+    newBunch.x = 10;
+
+    bunch_t *newBunch_p = &newBunch;
+    (*newBunch_p).x = 5; // This line and the one below it are exactly the same.
+    newBunch_p->x = 5;  // They both modify the member x of the struct that the pointer is pointing to.
+
     increaseBunch_p(&newBunch);
 
     while (1);
